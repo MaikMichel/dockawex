01b6ed5fa007ab21137d26cd080d156506d3f5ac
Maik Michel <maik.michel78@gmail.com>
2020-10-23 12:50:53 +0200

    Cleanup tomcat installation

--- infrastructure/docker/appsrv/scripts/install_tomcat.sh
+++ infrastructure/docker/appsrv/scripts/install_tomcat.sh
@@ -2,28 +2,20 @@
 
 cd /files
 
-echo "list files (1)"
-ls -l
-
-echo "tar -xzf $FILE_TOMCAT"
+echo "unpacking tomcat: tar -xzf $FILE_TOMCAT"
 tar -xzf $FILE_TOMCAT
-
-echo "list files (2)"
-ls -l
 
 FILE_TOMCAT_WITHOUT_EXT=${FILE_TOMCAT/.tar.gz/}
 echo "move /files/$FILE_TOMCAT_WITHOUT_EXT /tomcat"
 mv /files/$FILE_TOMCAT_WITHOUT_EXT /tomcat
 
-echo "ls /tomcat"
-ls -l /tomcat
 
 
 sed -i -e 's/password="secret"/password="'$TOM_PASSWORD'"/g' /scripts/tomcat-users.xml
 mv /scripts/tomcat-users.xml /tomcat/conf
 mv /scripts/tomcat-server.xml /tomcat/conf/server.xml
 mv /scripts/tomcat-web.xml /tomcat/conf/web.xml
-mv /scripts/tomcat-error.jsp /tomcat/conf/error.jsp
+cp /scripts/tomcat-error.jsp /tomcat/conf/error.jsp
 mv /scripts/tomcat-error.jsp /tomcat/webapps/error.jsp
 
 rm -rf /tomcat/webapps/docs
@@ -36,3 +28,5 @@ rm -rf /tomcat/webapps/ROOT
 mv /scripts/tomcat8 /etc/init.d/tomcat
 chmod 755 /etc/init.d/tomcat
 update-rc.d tomcat defaults  80 01
+
+echo "tomcat installed"
