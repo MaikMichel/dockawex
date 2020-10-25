4a0bb8b7e42dd09a19b9787996d407ae019cd7b1
Maik Michel <maik.michel78@gmail.com>
2020-10-25 13:09:54 +0100

    Hide tomcat version info from error pages

--- infrastructure/docker/appsrv/scripts/install_tomcat.sh
+++ infrastructure/docker/appsrv/scripts/install_tomcat.sh
@@ -10,19 +10,30 @@ echo "move /files/$FILE_TOMCAT_WITHOUT_EXT /tomcat"
 mv /files/$FILE_TOMCAT_WITHOUT_EXT /tomcat
 
 
-
-sed -i -e 's/password="secret"/password="'$TOM_PASSWORD'"/g' /scripts/tomcat-users.xml
-mv /scripts/tomcat-users.xml /tomcat/conf
-mv /scripts/tomcat-server.xml /tomcat/conf/server.xml
-mv /scripts/tomcat-web.xml /tomcat/conf/web.xml
-cp /scripts/tomcat-error.jsp /tomcat/conf/error.jsp
-mv /scripts/tomcat-error.jsp /tomcat/webapps/error.jsp
-
 rm -rf /tomcat/webapps/docs
 rm -rf /tomcat/webapps/examples
 rm -rf /tomcat/webapps/host-manager
 rm -rf /tomcat/webapps/manager
 rm -rf /tomcat/webapps/ROOT
+
+sed -i -e 's/password="secret"/password="'$TOM_PASSWORD'"/g' /scripts/tomcat-users.xml
+mv /scripts/tomcat-users.xml /tomcat/conf
+mv /scripts/tomcat-server.xml /tomcat/conf/server.xml
+mv /scripts/tomcat-web.xml /tomcat/conf/web.xml
+
+# ErrorPages taken from https://github.com/HttpErrorPages/HttpErrorPages
+mkdir /tomcat/webapps/ROOT
+mv /scripts/HTTP400.html /tomcat/webapps/ROOT/HTTP400.html
+mv /scripts/HTTP401.html /tomcat/webapps/ROOT/HTTP401.html
+mv /scripts/HTTP403.html /tomcat/webapps/ROOT/HTTP403.html
+mv /scripts/HTTP404.html /tomcat/webapps/ROOT/HTTP404.html
+mv /scripts/HTTP500.html /tomcat/webapps/ROOT/HTTP500.html
+mv /scripts/HTTP501.html /tomcat/webapps/ROOT/HTTP501.html
+mv /scripts/HTTP502.html /tomcat/webapps/ROOT/HTTP502.html
+mv /scripts/HTTP503.html /tomcat/webapps/ROOT/HTTP503.html
+mv /scripts/HTTP520.html /tomcat/webapps/ROOT/HTTP520.html
+mv /scripts/HTTP521.html /tomcat/webapps/ROOT/HTTP521.html
+mv /scripts/HTTP533.html /tomcat/webapps/ROOT/HTTP533.html
 
 
 mv /scripts/tomcat8 /etc/init.d/tomcat
@@ -30,3 +41,7 @@ chmod 755 /etc/init.d/tomcat
 update-rc.d tomcat defaults  80 01
 
 echo "tomcat installed"
+
+# /etc/init.d/tomcat start
+# /etc/init.d/tomcat stop
+# /etc/init.d/tomcat restart
