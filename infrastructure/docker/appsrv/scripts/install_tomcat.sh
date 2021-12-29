155696b0cee6897fc459f792cf1cbd34cd681d0b
Maik Michel <maik.michel78@gmail.com>
2021-12-29 16:43:31 +0100

    Fix: Remove hard coded reference to tomcat version

--- infrastructure/docker/appsrv/scripts/install_tomcat.sh
+++ infrastructure/docker/appsrv/scripts/install_tomcat.sh
@@ -6,9 +6,8 @@ exec 2>&1
 echo "unpacking tomcat"
 tar xzf /files/tomcat.tgz -C /tmp
 
-TOMCAT_VERSION=9.0.54
-echo "moving tomcat from /tmp/apache-tomcat-${TOMCAT_VERSION}  to /tomcat"
-mv /tmp/apache-tomcat-${TOMCAT_VERSION} /tomcat
+echo "moving tomcat from /tmp/apache-tomcat-*  to /tomcat"
+mv /tmp/apache-tomcat-* /tomcat
 
 echo "clearing up tomcat"
 rm /files/tomcat.tgz
